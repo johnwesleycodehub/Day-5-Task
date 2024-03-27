4fd7e2529467850d0d2bc92533622e193c26aaeb
johnwesleycodehub <johnwesley2307r@gmail.com>
2024-03-27 12:51:58 +0530

    Updated

--- script.js
+++ script.js
@@ -1,69 +1,68 @@
 //! Resume data on JSON Format
-/*
-let ownResume = {
-  personaldetails: {
-    name: "JOHN WESLEY R",
-    email: "johnwesley2307r@gamil.com",
-    phone: 6381301756,
-    degree: "B.E",
-    location: {
-      address: "Ponnagar,Mettur",
-      postalCode: 636401,
-      city: "Salem",
-      state: "Tamilnadu",
-      country: "India",
+
+let ownResume = [
+  {
+  "basics": {
+    "name": "John Wesley",
+    "email": "johnwesley2307r@gmail.com",
+    "phone": "9655291756",
+    "github url": "https://github.com/johnwesleycodehub",
+    "location": {
+      "address": "Ponnagar,Mettur",
+      "postalCode": "636401",
+      "city": "Salem",
+      "state": "Tamil Nadu",
+      "country": "India",
     },
-    profiles: [
-      {
-        github: "https://github.com/johnwesleycodehub",
-      },
-    ],
   },
-  work: [
+},
+],[
+  { "work": 
     {
-      company: "Tata Consultancy Services",
-      position: "Product Specialist",
-      startDate: "2022-07-21",
-      summary:
-        "Working as a Product Specialist in Google YouTube Tv operations",
+      "company":"Tata Consultancy Services",
+      "position":"Product Specialist",
+      "startDate":"2022-07-21",
+      "summary":"Working as a Product Specialist in Google YouTube Tv operations",
+    },
+  "education": 
+    {
+      "institution": "Bannari Amman Institute of Technology",
+      "department": "Automobile Engineering",
+      "studyType": "fulltime",
+      "batch start year": "2018",
+      "batch end year": "2022",
+      "gpa": 8.6,
+    },
+  };
+  ],
+  [  {"skills":
+    {
+      "name": "c++,javascript",
+      "level": "beginer",
+      "project": ["Automotive Cruise Control System"],
+    },
+  }, ],
+  [ {
+  "languages": [
+    {
+      "language": "Tamil,Enlish",
     },
   ],
-  education: [
+  "interests": [
     {
-      institution: "Bannari Amman Institute of Technology",
-      department: "Automobile Engineering",
-      studyType: "fulltime",
-      "batch start year": 2018,
-      "batch end year": 2022,
-      gpa: 8.6,
+      "interest": "Music composing,Music Teaching",
     },
   ],
-  skills: [
-    {
-      name: "c++,javascript",
-      level: "beginer",
-      project: ["Automotive Cruise Control System"],
-    },
-  ],
-  languages: [
-    {
-      language: "Tamil,Enlish",
-    },
-  ],
-  interests: [
-    {
-      name: "Music composing,Music Teaching",
-    },
-  ],
-}
+},
+],
 console.log(ownResume);
-*/
+
 
 //------------------------------------------------------------------------
 
 //! Iterate over all for loops (for, for in, for of, for each)
 
-let ownResume = {
+/*let ownResume = {
     personaldetails: {
       name: "JOHN WESLEY R",
       email: "johnwesley2307r@gamil.com",
@@ -121,5 +120,7 @@ let ownResume = {
   }
   for(var i=0;i<ownResume.personaldetails.length;i=i+1)
   {
-    console.log(ownResume.personaldetails[i]);
+    let obj=ownResume[i];
+    console.log(obj);
   }
+  */
